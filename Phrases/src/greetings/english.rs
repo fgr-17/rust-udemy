190f45e3a588666cb6ad96682f5e5bfba3918dc8
fgr-17 <rouxfederico@gmail.com>
2021-02-25 18:08:28 -0300

    Documentation

--- Phrases/src/greetings/english.rs
+++ Phrases/src/greetings/english.rs
@@ -1,2 +1,17 @@
+//! Este modulo tiene frases ejemplo
+//!
+//! # Examples
+//! ```
+//! let username = "John";
+//! prinln!("{}, {}!,
+//!     phrases::greetings::english::hello(),
+//!     username);
+//! ```
+//!
+
+
+// 3 slashes: documentacion md
+/// Applies to code that follows it
+/// In this case, it's our hello() function
 pub fn hello() -> String { "hello".to_string() }
 pub fn goodbye() -> String { "goodbye".to_string() }
